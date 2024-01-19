4965458fa2fe13d436ff11f2f259a848d4a2aaef
gomidx <lucaspavao2002@gmail.com>
2024-01-18 21:52:42 -0300

    chore(web): refactoring routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,37 +18,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Login
 Route::get('/', [AuthController::class, 'login'])->name('auth.login');
-Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('/login', [AuthController::class, 'loginPost'])->name('auth.login-post');
 
-// Registration
-Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
-Route::post('/register', [AuthController::class, 'registerPost'])->name('auth.register-post');
+Route::prefix('/login')->group(function () {
+    Route::get('/', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/', [AuthController::class, 'loginPost'])->name('auth.login-post');
+});
+
+Route::prefix('/register')->group(function () {
+    Route::get('/', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/', [AuthController::class, 'registerPost'])->name('auth.register-post');
+});
 
 Route::middleware(['auth'])->group(function () {
-    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.home');
 
-    // Sales
-    Route::get('/dashboard/sales', [SaleController::class, 'index'])->name('dashboard.sales');
-    Route::get('/dashboard/sale/create', [SaleController::class, 'createSale'])->name('dashboard.create-sale');
-    Route::post('/dashboard/sale/create', [SaleController::class, 'createSalePost'])->name('dashboard.create-sale-post');
+    Route::prefix('/dashboard')->group(function () {
+        Route::get('/sales', [SaleController::class, 'index'])->name('dashboard.sales');
 
-    // Email
-    Route::get('/dashboard/send-sales-email/{email}', [EmailController::class, 'sendEmailToAdministrator'])->name('dashboard.send-administrator-email');
-    Route::get('/dashboard/sellers/{id}/send-email/{email}', [EmailController::class, 'sendEmailToSeller'])->name('dashboard.send-seller-email');
+        Route::prefix('/sale')->group(function () {
+            Route::get('/create', [SaleController::class, 'createSale'])->name('dashboard.create-sale');
+            Route::post('/create', [SaleController::class, 'createSalePost'])->name('dashboard.create-sale-post');
+        });
 
-    // Sellers
-    Route::get('/dashboard/sellers', [SellerController::class, 'index'])->name('dashboard.sellers');
-    Route::get('/dashboard/seller/{id}/sales', [SaleController::class, 'getSellerSales'])->name('dashboard.sales-by-seller');
-    Route::get('/dashboard/seller/create', [SellerController::class, 'createSeller'])->name('dashboard.create-seller');
-    Route::post('/dashboard/seller/create', [SellerController::class, 'createSellerPost'])->name('dashboard.create-seller-post');
-    Route::get('/dashboard/seller/{id}', [SellerController::class, 'updateSeller'])->name('dashboard.edit-seller');
-    Route::put('/dashboard/seller/{id}', [SellerController::class, 'updateSellerPost'])->name('dashboard.edit-seller-post');
-    Route::delete('/dashboard/seller/{id}', [SellerController::class, 'deleteSeller'])->name('dashboard.delete-seller');
+        Route::get('/send-sales-email/{email}', [EmailController::class, 'sendEmailToAdministrator'])->name('dashboard.send-administrator-email');
 
-    // Logout
+        Route::prefix('/sellers')->group(function () {
+            Route::get('/{id}/send-email/{email}', [EmailController::class, 'sendEmailToSeller'])->name('dashboard.send-seller-email');
+            Route::get('/', [SellerController::class, 'index'])->name('dashboard.sellers');
+        });
+
+        Route::prefix('/seller')->group(function () {
+            Route::get('/{id}/sales', [SaleController::class, 'getSellerSales'])->name('dashboard.sales-by-seller');
+            Route::get('/create', [SellerController::class, 'createSeller'])->name('dashboard.create-seller');
+            Route::post('/create', [SellerController::class, 'createSellerPost'])->name('dashboard.create-seller-post');
+            Route::get('/{id}', [SellerController::class, 'updateSeller'])->name('dashboard.edit-seller');
+            Route::put('/{id}', [SellerController::class, 'updateSellerPost'])->name('dashboard.edit-seller-post');
+            Route::delete('/{id}', [SellerController::class, 'deleteSeller'])->name('dashboard.delete-seller');
+        });
+
+    });
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
